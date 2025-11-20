ab2f0a66a93efad083f986fabcc6d58d95074b97
Abe Megahed <abem@tds.net>
2025-11-20 13:49:48 -0600

    Added utility run scripts.

---
+++ run.sh
@@ -0,0 +1,7 @@
+# This command will run the application.  
+#
+# After running this command, open your browser to "localhost" to view the application.
+# It may take a while to start.  Wait until the long stream of messages in your command
+# line console comes to an end before viewing the application in your browser.
+#
+docker compose up
---
+++ start.sh
@@ -0,0 +1,7 @@
+# This command will rebuild and restart the application.
+#
+# After running this command, open your browser to "localhost" to view the application.
+# It may take a while to build.  Wait until the long stream of messages in your command
+# line console comes to an end before viewing the application in your browser.
+#
+docker compose up --build
